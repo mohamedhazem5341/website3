a871b3d17600bafe5f6dcf19849ede1d010f6b9f
Mohamed Hazem <77168567+mohamedhazem5341@users.noreply.github.com>
2025-02-19 22:09:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const body = document.getElementById('body')
+const darkmode = document.querySelector('.dark-mode')
+
+darkmode.addEventListener('click', function (eo) {
+    body.classList.toggle('dark')
+    body.classList.toggle('light')
+})
+
+
+
+
+
+
+
+
+
+
